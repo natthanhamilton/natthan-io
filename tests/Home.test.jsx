e9f2d37804e74a2c66fd7d2cd5f45275ac5b87ca
natthanhamilton <natthanhamilton@gmail.com>
2018-07-06 14:43:30 -0700

    unit test edit

--- tests/Home.test.jsx
+++ tests/Home.test.jsx
@@ -1,57 +1,41 @@
-import React from "react";
-import {
-  Provider
-} from 'react-redux';
+import React from 'react';
+import { Provider } from 'react-redux';
 import jest from 'jest';
-import Enzyme, {
-  mount
-} from 'enzyme';
+import Enzyme, { mount } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
-import cloudinary from 'cloudinary'
-import {
-  I18nextProvider,
-  reactI18nextModule
-} from 'react-i18next';
+import { I18nextProvider, reactI18nextModule } from 'react-i18next';
 import i18n from 'i18next';
 import Backend from 'i18next-xhr-backend';
 import LanguageDetector from 'i18next-browser-languagedetector';
 
-import Home from "../src/app/components/Home";
-import {
-  JestStore
-} from '../src/app/store';
+import Home from '../src/app/components/Home';
+import { JestStore } from '../src/app/store';
 
 i18n
-  .use( Backend )
-  .use( LanguageDetector )
-  .use( reactI18nextModule )
-  .init( {
+  .use(Backend)
+  .use(LanguageDetector)
+  .use(reactI18nextModule)
+  .init({
     fallbackLng: 'cimode',
     debug: false,
     saveMissing: false,
-
     react: {
       wait: false,
-      nsMode: 'fallback', // set it to fallback to let passed namespaces to translated hoc act as fallbacks
-    },
-  } );
+      nsMode: 'fallback'
+    }
+  });
 
-cloudinary.config( {
-  cloud_name: 'natthanhamilton',
-  api_key: '621237428861119',
-  api_secret: 'QIqk-HNw3dDdWRz-iHFUgBjeE3Y'
-} );
-
-Enzyme.configure( {
+Enzyme.configure({
   adapter: new Adapter()
-} );
+});
 
-describe( "homeome", () => {
+describe('home', () => {
   let props;
   let mountedHome;
 
   const home = () => {
-    if ( !mountedHome ) {
+    n;
+    if (!mountedHome) {
       mountedHome = mount(
         <I18nextProvider i18n={i18n}>
           <Provider store={JestStore}>
@@ -61,19 +45,16 @@ describe( "homeome", () => {
       );
     }
     return mountedHome;
-  }
+  };
 
-  beforeEach( () => {
-
-    props = {
-      wallpaperPath: undefined,
-    };
+  beforeEach(() => {
+    props = { wallpaperPath: undefined };
 
     mountedHome = undefined;
-  } );
+  });
 
-  it( "always renders a div", () => {
-    const divs = home().find( "div" );
-    expect( divs.length ).toBeGreaterThan( 0 );
-  } );
-} );
+  test('always renders a div', () => {
+    const div = home().find('div');
+    expect(div.length).toBeGreaterThan(0);
+  });
+});
