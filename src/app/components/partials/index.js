0e9ad6874224f36ec599c9336b00c44414675de7
natthanhamilton <natthanhamilton@gmail.com>
2018-07-06 14:43:49 -0700

    index edit

---
+++ src/app/components/partials/index.js
@@ -0,0 +1,25 @@
+import Layout from './Layout';
+import Portfolio from './Portfolio';
+import SkillsBreakdown from './SkillsBreakdown';
+import Skills from './Skills';
+import Experience from './Experience';
+import Person from './Person';
+import About from './About';
+import Education from './Education';
+import Footer from './Footer';
+import MyStory from './MyStory';
+import GenerateSkillsList from './GenerateSkillsList';
+
+export {
+  Layout,
+  Portfolio,
+  SkillsBreakdown,
+  Skills,
+  Experience,
+  Person,
+  About,
+  Education,
+  Footer,
+  MyStory,
+  GenerateSkillsList
+};
