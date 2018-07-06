3f883e82326383a552c40465b08f4a4f0f8e3397
natthanhamilton <natthanhamilton@gmail.com>
2018-07-06 16:02:11 -0700

    webpack edits

--- webpack.config.js
+++ webpack.config.js
@@ -1,28 +1,24 @@
-const path = require( 'path' );
-const webpack = require( 'webpack' );
-const CleanWebpackPlugin = require( 'clean-webpack-plugin' );
-const CopyWebpackPlugin = require( 'copy-webpack-plugin' );
-const HtmlWebpackPlugin = require( 'html-webpack-plugin' );
-const ExtractTextPlugin = require( "extract-text-webpack-plugin" );
-const CompressionPlugin = require( "compression-webpack-plugin" )
-const Dotenv = require( 'dotenv-webpack' );
-const {
-  GenerateSW
-} = require( 'workbox-webpack-plugin' );
-const BrotliPlugin = require( 'brotli-webpack-plugin' );
+const path = require('path');
+const webpack = require('webpack');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const Dotenv = require('dotenv-webpack');
+const { GenerateSW } = require('workbox-webpack-plugin');
+const BrotliPlugin = require('brotli-webpack-plugin');
 
-const BUILD_DIR = path.resolve( __dirname, 'dist' );
-const DIST_DIR = path.resolve( __dirname, 'dist/app' );
-const APP_DIR = path.resolve( __dirname, 'src/app' );
-const SRC_DIR = path.resolve( __dirname, 'src' );
+const BUILD_DIR = path.resolve(__dirname, 'dist');
+const DIST_DIR = path.resolve(__dirname, 'dist/app');
+const APP_DIR = path.resolve(__dirname, 'src/app');
+const SRC_DIR = path.resolve(__dirname, 'src');
 
-module.exports = ( env ) => {
+module.exports = env => {
   const isDev = env == 'development';
 
   return {
     mode: env,
 
-    context: path.resolve( __dirname ),
+    context: path.resolve(__dirname),
     entry: {
       main: APP_DIR + '/app.jsx',
       vendor: [
@@ -37,22 +33,18 @@ module.exports = ( env ) => {
         'redux',
         'react-router-redux',
 
-        'material-ui',
-        'material-ui-icons',
+        '@material-ui/core',
+        '@material-ui/icons',
         'cloudinary'
-       ]
+      ]
     },
-    // Needed for dependencies
     node: {
-      // console: true,
       fs: 'empty',
       net: 'empty',
       tls: 'empty'
     },
     output: {
       path: DIST_DIR,
-      //filename: isDev ? 'js/[name].bundle.js' : 'js/[name].[hash:6].bundle.js',
-      //chunkFilename: isDev ? 'js/[id].chunk.js' : 'js/[id].[chunkhash:6].chunk.js',
 
       filename: 'js/[name].[hash:6].bundle.js',
       chunkFilename: 'js/[id].[chunkhash:6].chunk.js',
@@ -66,29 +58,25 @@ module.exports = ( env ) => {
           test: /\.jsx?$/,
           loader: 'babel-loader',
           query: {
-            "presets": [
-            [ "env", {
-                modules: false
-            } ], "stage-2", "react"
-          ],
-            "plugins": [ "react-hot-loader/babel" ]
+            presets: [
+              [
+                'env',
+                {
+                  modules: false
+                }
+              ],
+              'stage-2',
+              'react'
+            ],
+            plugins: ['react-hot-loader/babel']
           },
-          exclude: [ /node_modules/ ]
-      }
-
-      /*, {
-          test: /\.(css|sass|scss)$/,
-          use: ExtractTextPlugin.extract( {
-            fallback: 'style-loader',
-            use: 'css-loader!sass-loader'
-          } )
+          exclude: [/node_modules/]
         }
-      */
-     ]
+      ]
     },
     resolve: {
-      extensions: [ '.js', '.jsx', '.json' ],
-      modules: [ APP_DIR, 'node_modules' ]
+      extensions: ['.js', '.jsx', '.json'],
+      modules: [APP_DIR, 'node_modules']
     },
     devServer: {
       contentBase: DIST_DIR,
@@ -97,55 +85,43 @@ module.exports = ( env ) => {
       publicPath: '/',
 
       historyApiFallback: true,
-      //contentBase: path.join(__dirname, "dist"),
-      stats: "minimal"
+      stats: 'minimal'
     },
-    devtool: "cheap-module-source-map",
-    performance: {
-      hints: "warning",
-      maxAssetSize: 200000,
-      maxEntrypointSize: 400000
-    },
-    //externals: ["react"],
-    stats: "errors-only",
     plugins: [
       // Environment variables for the app
       new Dotenv(),
 
       // Inject the build environment
-      new webpack.DefinePlugin( {
+      new webpack.DefinePlugin({
         env: {
           NODE_ENV: isDev ? 'development' : 'production'
-        },
-      } ),
+        }
+      }),
 
       // Delete current build
-      new CleanWebpackPlugin( [ BUILD_DIR ] ),
-
-      /*
-      // Build CSS
-      new ExtractTextPlugin( {
-        filename: 'style-[hash:6].css',
-        allChunks: true
-      } ),
-      */
+      new CleanWebpackPlugin([BUILD_DIR]),
 
       // Move to Dist
-      new CopyWebpackPlugin( [
+      new CopyWebpackPlugin([
         {
           from: './src/index.html'
-      }, {
+        },
+        {
           from: './src/assets',
           to: './assets'
-      }, {
+        },
+        {
           from: './distAssets',
           to: '../'
-      } ] ),
+        }
+      ]),
 
-      new HtmlWebpackPlugin( {
+      new HtmlWebpackPlugin({
         template: SRC_DIR + '/index.html'
-      } ),
-/*
+      })
+    ]
+
+    /*
       new webpack.optimize.AggressiveMergingPlugin(),
       new CompressionPlugin( {
         asset: '[path].gz[query]',
@@ -163,12 +139,11 @@ module.exports = ( env ) => {
       new GenerateSW( {
         globDirectory: DIST_DIR,
         globPatterns: [ '**/
-      /*.{html,js}' ],
+    /*.{html,js}' ],
               swDest: 'sw.js',
               clientsClaim: true,
               skipWaiting: true,
             } )
       */
-    ]
-  }
+  };
 };
